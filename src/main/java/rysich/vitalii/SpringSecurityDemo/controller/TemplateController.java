6df6db2c447ad40ef435149328ccc92f3b0053f8
Vitalii <vitaliy.rysich@gmail.com>
2020-11-26 01:30:52 +0100

    Own login page

---
+++ src/main/java/rysich/vitalii/SpringSecurityDemo/controller/TemplateController.java
@@ -0,0 +1,17 @@
+package rysich.vitalii.SpringSecurityDemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Controller
+@RequestMapping("/")
+public class TemplateController {
+
+    @GetMapping("login")
+    public String getLoginView() {
+        return "login";
+    }
+}
